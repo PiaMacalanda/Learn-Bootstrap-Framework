093c70e50765005f62c5bba7c96b8d2381460728
Pia Katleya V. Macalanda <piav.macalanda@gmail.com>
2025-03-02 08:26:36 +0800

    Update index.

--- index.php
+++ index.php
@@ -21,17 +21,17 @@ $activities = [
 
     <!-- commenting because practicing non-CDN/local-based bootstrap o(*￣▽￣*)ブ -->
         <!-- Latest compiled and minified CSS -->
-        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
         <!-- Optional theme -->
-        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"> -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
         <!-- Latest compiled and minified JavaScript -->
-        <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-        <link rel= "stylesheet" href="css/bootstrap.min.css">
+        <!-- <link rel= "stylesheet" href="css/bootstrap.min.css">
         <script src="js\jquery.js"></script>
-        <script src="js\bootstrap.js"></script>
+        <script src="js\bootstrap.js"></script> -->
         
     <style>
         body {
